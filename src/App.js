a3c50808c7910a7479204563629f5c4835425d5c
Harsh Sharma <harshsharma20503@gmail.com>
2023-02-10 01:12:02 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets build the MERN TINDER clone</h1>
+
+      {/* Header */}
+      {/* TinderCards */}
+      {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
